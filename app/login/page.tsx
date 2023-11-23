1173b513b10636e33bf133a81274b67bc3a7425d
kmrashidbd <itskmrbd@gmail.com>
2023-11-23 12:35:37 +0600

    second commit

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      this is login
+      <button className="btn btn-neutral">Neutral</button>
+    </div>
+  )
+}
+
+export default Login
